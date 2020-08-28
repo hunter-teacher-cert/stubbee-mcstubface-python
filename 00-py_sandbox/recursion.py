4c16f8716d3fcb132f8962306d26b1bc55800a17
tofr <mykolyk@stuycs.org>
2020-08-28 11:46:06 -0400

    +py sample code ' ;

---
+++ 00-py_sandbox/recursion.py
@@ -0,0 +1,13 @@
+# Clyde "Thluffy" Sinclair
+# Aug 2020
+
+def factorial(n):
+    return 1
+
+def fib(n):
+    return 1
+
+print("Good News Everyone!")
+print(f"1! = {factorial(1)}" )
+print(f"fib(1) = {fib(1)}" )
+
